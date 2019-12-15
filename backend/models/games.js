9dfd5298b3af3b558af9c3755dfbf4ca60d3743a
ryanwolfen7 <wolfen7lada@gmail.com>
2019-12-15 07:27:13 +0000

    set up my post route and added a model

---
+++ backend/models/games.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  players: {
+    type: Number,
+    required: true
+  },
+  boxArt: {
+    type: String,
+    required: true
+  },
+  platforms: {
+    type: Object,
+    required: true
+  },
+});
+
+module.exports = mongoose.model('Game', gameSchema)
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const Game = require('../models/games')
 
 router.get('/', (req, res) => {
   res.send('hello')
 })
 
-router.post('/addGame', (req, res) => {
-  console.log(req)
-  res.send({data: req})
+router.post('/addGame', async (req, res) => {
+  console.log('hit')
+  const { name, genre, releaseDate, players, boxArt, platforms } = req.body
+  const game = new Game({
+    name, genre, releaseDate, players, boxArt, platforms  
+  })
+
+  try { 
+    const newGame = await game.save()
+    res.status(201).json(newGame)
+  } catch (error) {
+    res.status(400).json({ message: err.message })
+  }
 })
 
 module.exports = router
